dd9b06010eebddf7cfe3f6ddb7e51ca67ae1b377
滝口結 <[525tkg@gmail.com]>
2022-07-23 13:58:48 +0900

    資格のアニメーション変更 メニューOK

--- js/app.js
+++ js/app.js
@@ -234,7 +234,57 @@ $(window).scroll(function () {
 
 
 
+// メニュー
+$(".openbtn1").click(function () {//ボタンがクリックされたら
+  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
 
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
+
+
+
+
+// ランダムに変化するアルファベットからのテキスト
+var arr = []
+//初期値の設定
+function TypingInit() {
+  $('.js_typing').each(function (i) { //js_typingクラスを全て処理をおこなう
+    arr[i] = new ShuffleText(this);//動作させるテキストを配列に格納
+  });
+}
+//スクロールした際のアニメーションの設定
+function TypingAnime() {
+  $(".js_typing").each(function (i) {
+    var elemPos = $(this).offset().top - 50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      if(!$(this).hasClass("endAnime")){//endAnimeのクラスがあるかチェック
+        arr[i].start();//配列で登録テキストのアニメーションをおこなう
+        arr[i].duration = 800;//テキストが最終変化するまでの時間※規定値600
+        $(this).addClass("endAnime");//１度アニメーションした場合はendAnimeクラスを追加
+      }
+    }else{
+      $(this).removeClass("endAnime"); //範囲外にスクロールした場合はendAnimeのクラスを削除
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  TypingAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  TypingInit(); //初期設定
+  TypingAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
 
 
 

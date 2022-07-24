5ada1e8cef2df2f6db809ed0668a2ca954812660
滝口結 <[525tkg@gmail.com]>
2022-07-24 18:46:58 +0900

    メニューの画像回転してるやつ削除 余暇時間　bgアニメーションOK ニュース　bgアニメーションOK

--- js/app.js
+++ js/app.js
@@ -172,33 +172,12 @@ window.onload = function() {
 
 
 
-
-// 起動中アイコンをエラーモードに
-// https://seiai.ed.jp/sys/text/htm4/chp09/h09a03.html
-function changingicon_errormode(){
-  document.getElementById("changingicon_area").setAttribute("src","/image/エラーアイコン.png");
-  $('.crush-image').each(function () {
-    
-    $(this).addClass("crush-imageOn");
-    var elemPos = $(this).offset().top - 50;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-  });
-
-}
-
-
-
-
-
-
 // 文字がスッと表示される 上から下に
 function markerControl() {
   $('.marker').each(function () {
     var elemPos = $(this).offset().top - 50;
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
-    console.log(elemPos, scroll, windowHeight)
     if (scroll >= elemPos - windowHeight + 380) {//画面中間でマーカー引かれる様に調整
       $(this).addClass("bgcOn");
 
@@ -290,6 +269,65 @@ $(window).on('load', function () {
 
 
 
+// テキスト１文字ずつ回転
+// rollAnimeにrollというクラス名を付ける定義
+function RollAnimeControl() {
+  $('.rollAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var childs = $(this).children();  //rollAnimeの子要素を取得
+    if (scroll >= elemPos - windowHeight) {
+      $(childs).each(function (i) {   //子要素を1つ1つ処理をおこなう
+        if (i < 10) {         //10未満の場合
+        $(this).css("transition-delay","."+i+"s");  //子要素にcsstransition-delayを追加
+        }else {             //10以上の場合
+          var n = i / 10;       //ミリ秒指定なので10で割る
+          $(this).css("transition-delay",n+"s");  //子要素にcsstransition-delayを追加
+        }
+      });
+      
+      $(this).addClass("roll"); //rollというアニメーションクラスを付与
+
+    } else {
+      $(childs).each(function () {    //子要素を1つ1つ処理をおこなう
+        $(this).css("transition-delay","0s");//子要素にcsstransition-delayの秒を0とする
+      });
+      $(this).removeClass("roll");//rollというアニメーションクラスを除去
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  RollAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  //spanタグを追加する
+  var element = $(".rollAnime");
+  element.each(function () {
+    var text = $(this).text();
+    var textbox = [];
+    text.split('').forEach(function (t, i) {
+      if (t !== " ") {
+        if (i < 10) {
+          textbox += '<span style="transition-delay:.' + i + 's;">' + t + '</span>';
+        } else {
+          var n = i / 10;
+          textbox += '<span style="transition-delay:' + n + 's;">' + t + '</span>';
+        }
+
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+  });
+
+  RollAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
 
 
 
@@ -300,9 +338,23 @@ $(window).on('load', function () {
 
 
 
+// 1文字ずつランダムに表示
+function bgcNewsControl() {
+  $('.bgc-news').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight + 100) {
+      $(this).addClass("bgcOn");
+      console.log("OK")
 
+    } else {
+      $(this).removeClass("bgcOn");
+    }
+  });
+}
 
-
-
-
-
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  bgcNewsControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述

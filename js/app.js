c6e09980f67dea7e94f8617ba82e38260c5c3798
滝口結 <[525tkg@gmail.com]>
2022-12-04 09:30:49 +0900

    no message

--- js/app.js
+++ js/app.js
@@ -1,60 +1,47 @@
-// エラーログ非表示
-// https://www.kaasan.info/archives/1610
-function TigilError() {
-	return true;
-}
-window.onerror = TigilError;
-
-
-
 // メニュー
-$(".openbtn1").click(function () {//ボタンがクリックされたら
-  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+$(".openbtn1").click(function () {
+  $(this).toggleClass('active');
+    $("#g-nav").toggleClass('panelactive');
 });
 
-$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+$("#g-nav a").click(function () {
+    $(".openbtn1").removeClass('active');
+    $("#g-nav").removeClass('panelactive');
 });
 
 
 
 // テキストランダム変換
 var arr = []
-//初期値の設定
+var ShuffleText = 0
+
 function TypingInit() {
-  $('.js_typing').each(function (i) { //js_typingクラスを全て処理をおこなう
-    arr[i] = new ShuffleText(this);//動作させるテキストを配列に格納
+  $('.js_typing').each(function (i) {
+    arr[i] = new ShuffleText(this);
+
   });
 }
-//スクロールした際のアニメーションの設定
 function TypingAnime() {
   $(".js_typing").each(function (i) {
-    var elemPos = $(this).offset().top - 50;//要素より、50px上の
+    var elemPos = $(this).offset().top - 50;
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
     if (scroll >= elemPos - windowHeight) {
-      if(!$(this).hasClass("endAnime")){//endAnimeのクラスがあるかチェック
-        arr[i].start();//配列で登録テキストのアニメーションをおこなう
-        arr[i].duration = 800;//テキストが最終変化するまでの時間※規定値600
-        $(this).addClass("endAnime");//１度アニメーションした場合はendAnimeクラスを追加
+      if(!$(this).hasClass("endAnime")){
+        arr[i].start();
+        arr[i].duration = 800;
+        $(this).addClass("endAnime");
       }
     }else{
-      $(this).removeClass("endAnime"); //範囲外にスクロールした場合はendAnimeのクラスを削除
+      $(this).removeClass("endAnime");
     }
   });
 }
-
-// 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
-  TypingInit(); //初期設定
-  TypingAnime();/* アニメーション用の関数を呼ぶ*/
-});// ここまで画面をスクロールをしたら動かしたい場合の記述
-
-// 画面が読み込まれたらすぐに動かしたい場合の記述
+  TypingInit();
+  TypingAnime();
+});
 $(window).on('load', function () {
-  TypingInit(); //初期設定
-  TypingAnime();/* アニメーション用の関数を呼ぶ*/
-});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
-
+  TypingInit();
+  TypingAnime();
+});
